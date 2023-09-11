e66731cb9e1466df7389d0aad3b55a2a8b6bd640
Nagapriyankar <nagapriyankar@gmail.com>
2023-09-10 17:44:38 -0700

    removed additional html elements

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 /* elements selection */
-let cont1 = document.getElementById('container1')
-let cont2 = document.getElementById('container2')
+let cont1 = document.getElementById('container')
 /* create html elements */
 let countdown = document.createElement('h3')
 countdown.id = 'countdown'
 cont1.append(countdown)
-let statement = document.createElement('h3')
-statement.id = 'statement'
-cont2.append(statement)
+
 
 
 window.onload = function () {
@@ -31,8 +28,7 @@ window.onload = function () {
                                     setTimeout(function () {
                                         countdown.innerHTML = 1;
                                         setTimeout(function () {
-                                            countdown.style.visibility = "hidden";
-                                            statement.innerHTML = 'Happy Independance Day';
+                                            countdown.innerHTML = 'Happy Independance Day';
                                         }, 1000);
                                     }, 1000);
                                 }, 1000);
